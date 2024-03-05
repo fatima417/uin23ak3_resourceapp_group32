720a7c4bcbadfa3a248a4211a33cb72f68a51c53
fatima417 <143400343+fatima417@users.noreply.github.com>
2024-03-05 21:39:34 +0100

    set up the initail layout

---
+++ resource-archive/src/layout/ResourceLayout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../style/layout.css"
+
+const ResourceLayout = ()=>{
+
+    return(
+    <div id="container">
+      <div id="wrapper">
+        <header>
+          <main>
+            <nav>
+              <ul>
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JAVASCRIPT</a></li>
+                <li><a href="#">REACT</a></li>
+                <li><a href="#">SANITY AND HEADLESS CMS</a></li>
+              </ul>
+            </nav>
+
+            <div id="content-area">
+             
+            </div>
+          </main>
+        </header>
+      </div>
+    </div>
+    )
+}
+
+
+export default ResourceLayout;
